2a57d9e7b375c5eed6809e50f36934fd47ae3800
Aleksey Salnikov <me@iamsalnikov.ru>
2018-08-03 11:03:41 +0300

    Http handler for creating a new question

--- web.go
+++ web.go
@@ -24,6 +24,7 @@ func (s *Server) Run() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/version", s.versionHandler).Methods(http.MethodGet)
+	r.HandleFunc("/", s.insertQuestionHandler).Methods(http.MethodPost)
 	r.HandleFunc("/{id:[0-9]+}", s.getQuestionHandler).Methods(http.MethodGet)
 
 	http.ListenAndServe(s.addr, r)
@@ -62,5 +63,36 @@ func (s *Server) getQuestionHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	encoder.Encode(q)
+}
+
+func (s *Server) insertQuestionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+
+	var q Question
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&q)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	q.ID = 0
+	id, err := s.qs.Put(q)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	q.ID = id
+
+	w.WriteHeader(http.StatusOK)
 	encoder.Encode(q)
 }
