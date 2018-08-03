5c3a63a58da59111612a509f12b5a97ce5faaa08
Aleksey Salnikov <me@iamsalnikov.ru>
2018-08-03 10:46:10 +0300

    Http handler for getting question

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/boltdb/bolt"
 	"github.com/joho/godotenv"
 )
 
@@ -12,4 +14,14 @@ func main() {
 		log.Fatalf("Couldn't load dotenv: %s", err)
 	}
 
+	db, err := bolt.Open(os.Getenv("DB_PATH"), 0600, nil)
+	if err != nil {
+		log.Fatalf("Couldn't load database file (%s): %s", os.Getenv("DB_PATH"), err)
+	}
+	defer db.Close()
+
+	qs := NewQuestionStorage(db)
+
+	server := NewServer(os.Getenv("HTTP_BIND"), qs)
+	server.Run()
 }
--- web.go
+++ web.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -22,12 +23,13 @@ func NewServer(addr string, qs *QuestionStorage) *Server {
 func (s *Server) Run() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/version", versionHandler).Methods(http.MethodGet)
+	r.HandleFunc("/version", s.versionHandler).Methods(http.MethodGet)
+	r.HandleFunc("/{id:[0-9]+}", s.getQuestionHandler).Methods(http.MethodGet)
 
 	http.ListenAndServe(s.addr, r)
 }
 
-func versionHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) versionHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
@@ -35,4 +37,30 @@ func versionHandler(w http.ResponseWriter, r *http.Request) {
 	encoder.Encode(map[string]string{
 		"version": version,
 	})
+}
+
+func (s *Server) getQuestionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	q, err := s.qs.Get(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	encoder.Encode(q)
 }
