2dcfe3ec146413c84b3086bf4707dfdd351386b0
Aleksey Salnikov <me@iamsalnikov.ru>
2018-08-03 18:50:17 +0300

    Update

--- web.go
+++ web.go
@@ -5,14 +5,17 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
+// Server struct represens a server
 type Server struct {
 	addr string
 	qs   *QuestionStorage
 }
 
+// NewServer creates a new server
 func NewServer(addr string, qs *QuestionStorage) *Server {
 	return &Server{
 		addr: addr,
@@ -20,14 +23,16 @@ func NewServer(addr string, qs *QuestionStorage) *Server {
 	}
 }
 
+// Run starts a server
 func (s *Server) Run() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/version", s.versionHandler).Methods(http.MethodGet)
 	r.HandleFunc("/", s.insertQuestionHandler).Methods(http.MethodPost)
 	r.HandleFunc("/{id:[0-9]+}", s.getQuestionHandler).Methods(http.MethodGet)
+	r.HandleFunc("/{id:[0-9]+}", s.updateQuestionHandler).Methods(http.MethodPut)
 
-	http.ListenAndServe(s.addr, r)
+	http.ListenAndServe(s.addr, handlers.CORS()(r))
 }
 
 func (s *Server) versionHandler(w http.ResponseWriter, r *http.Request) {
@@ -92,6 +97,48 @@ func (s *Server) insertQuestionHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	q.ID = id
+
+	w.WriteHeader(http.StatusOK)
+	encoder.Encode(q)
+}
+
+func (s *Server) updateQuestionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	decoder := json.NewDecoder(r.Body)
+	vars := mux.Vars(r)
+
+	id, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(map[string]string{
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	var q Question
+	err = decoder.Decode(&q)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(map[string]string{
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	q.ID = id
+	_, err = s.qs.Put(q)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(map[string]string{
+			"error": err.Error(),
+		})
+
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 	encoder.Encode(q)
